2100eecdadf5e43fd232552f90f772dd785f90f8
christianyitzak <christianyitzak@gmail.com>
2025-03-10 11:00:11 +0700

    add hover color and make dropdown clickable for dekstop

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 const btnOpenDropdown = document.querySelector('.button-menu');
 const btnCloseDropdown = document.querySelector('.close-dropdown');
 const mobileDropdownMenu = document.querySelector('.mobile-dropdown');
+const dekstopDropdownBtn = document.querySelector('.dropdown-btn');
+const dekstopDropdownMenu = document.querySelector('.dropdown');
 
 btnOpenDropdown.addEventListener('click', () => {
-    if (mobileDropdownMenu.classList.contains('hidden')) {
-        mobileDropdownMenu.classList.remove('hidden');
-    }
+    mobileDropdownMenu.classList.toggle('hidden');
 })
 
 btnCloseDropdown.addEventListener('click', () => {
-    if (!mobileDropdownMenu.classList.contains('hidden')) {
-        mobileDropdownMenu.classList.add('hidden');
-    }
+    mobileDropdownMenu.classList.toggle('hidden');
+})
+
+dekstopDropdownBtn.addEventListener('click', () => {
+    dekstopDropdownMenu.classList.toggle('hidden');
 })
